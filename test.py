8bb5b87b0004077b336bacaf63ef6e72ed25c1e8
Wojciech <hcketjow@gmail.com>
2021-09-26 20:20:30 +0200

    26.09.2021

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 # Typy zmiennych: 
 a = 5 #liczba całkowita (int)
 b = 4.3 #liczba zmiannoprzecinkowa (float)
-imie = 'Wojciech' #ciąg znaków (string)   
-mleko = True #true or false (bool)
+imie = 'Wojciech' #ciąg znaków (string)    
+mleko = True #true or false (bool) 
---
+++ vat.py
@@ -0,0 +1,8 @@
+canaNettoJava = 10
+canaNettoAjax = 20
+VAT = 23
+obliczonyVAT = (1 + VAT / 100)
+
+cenaBruttoJava = canaNettoJava * obliczonyVAT
+cenaBruttoAjax = canaNettoAjax * obliczonyVAT
+
